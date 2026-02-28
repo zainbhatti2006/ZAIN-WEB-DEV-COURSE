017a44fc0248deb09555cb939265fce0c76492ff
Zainbhatti2006 <zainbhatti3666@gmail.com>
2026-03-01 00:16:11 +0500

    Lecture 54
     In this lecture, we will learn about JavaScript and how to use it in HTML files

---
+++ Lecture 54/script.js
@@ -0,0 +1,20 @@
+alert("Hello World!");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is look good...")
+
+var a = prompt("Enter a number");
+var isTrue = confirm("Are you sure you want to leave this page or you want to leave this page?");
+
+if(isTrue){
+    console.log("Your computer is blasting")
+}
+else{
+    console.log("Your computer is not blasting")
+
+}
+console.log("The number you entered is: " + a)
+
+document.title = "Hey I am good";
+document.body.style.backgroundColor = "red";
